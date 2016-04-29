20edf965caaf898839546ee321ef4f5e5321dc15
ledrou_83 <simondrouin4@gmail.com>
2016-04-29 18:37:06 -0400

    fix pre post assertions

--- lib/gitsearch/Log.rb
+++ lib/gitsearch/Log.rb
@@ -1,10 +1,11 @@
 class Log
+  LOG_DIRECTORY_NAME = "logs"
   LOG_FILE_EXT = ".tp3Log"
 
   attr_reader :filename
   def initialize
     @id = SecureRandom.uuid
-    @filename = File.join(@id, LOG_FILE_EXT)
+    @filename = File.join(LOG_DIRECTORY_NAME, @id + LOG_FILE_EXT)
   end
 
   def log_id(id)
@@ -14,7 +15,7 @@ class Log
 
   def log(ids)
     File.open(@filename, "w") do |o|
-      ids.each { |id| o << id }
+      ids.each { |id| o.puts id }
     end
   end
 
