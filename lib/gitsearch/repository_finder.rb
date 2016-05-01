25f7dcb3cdd1b7d2a139fafd9f5deaae84333f3d
mathfarm <mathurin05@gmail.com>
2016-05-01 17:08:25 -0400

    fix du hash et condition language

--- lib/gitsearch/repository_finder.rb
+++ lib/gitsearch/repository_finder.rb
@@ -3,7 +3,7 @@ require "gitsearch/external_service/github_dispatcher"
 class RepositoryFinder
   def search(query, language= nil)
     params = "q=#{query}"
-    params += "+language:#{language}" if language.nil?
+    params += "+language:#{language}" if language
 
     # Il pourrait y avoir un loop d'offsets ici car Github ne retourne que 100 items à la fois.
     # Le but du projet n'étant pas de remplir une base de donnée aberrante. 100 items suffiront.
