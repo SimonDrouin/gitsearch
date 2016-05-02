b24e37d292b7660a67f00c57c5f020f150e53faf
ledrou_83 <simondrouin4@gmail.com>
2016-05-02 09:30:47 -0400

    setup test env

--- Gemfile
+++ Gemfile
@@ -2,4 +2,5 @@ source 'https://rubygems.org'
 gemspec
 
 gem "rest-client"
+gem "minitest"
 gem "oj"
---
+++ test/gitsearch_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+
+describe Gitsearch do
+
+  describe "when asked about cheeseburgers" do
+    it "must respond positively" do
+      true
+    end
+  end
+end
